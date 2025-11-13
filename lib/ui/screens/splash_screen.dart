b3b425e051db073642b2b7b3ec751573f585eeab
kamrul <kakamrul2000@gmail.com>
2025-11-13 23:05:16 +0600

    spashScreen_To_signInScreen

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_11/ui/screens/sign_in_screen.dart';
+import 'package:flutter_application_11/ui/utilities/asset_paths.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+
 class splashScreen extends StatefulWidget {
   const splashScreen({super.key});
 
@@ -9,10 +12,35 @@ class splashScreen extends StatefulWidget {
 
 class _splashScreenState extends State<splashScreen> {
   @override
+  void initState() {
+    super.initState();
+    _moveToNextScreen();
+  }
+
+
+  Future<void> _moveToNextScreen() async {
+    await Future.delayed(Duration(seconds: 2));
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => signInScreen()),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: SvgPicture.asset('assets/images/logo.svg',height: 60,width: 60,),
+      body: Stack(
+        children: [
+          SvgPicture.asset(
+            AssetPaths.backgroundSvg,
+            width: double.maxFinite,
+            height: double.maxFinite,
+            fit: BoxFit.cover,
+          ),
+          Center(
+            child: SvgPicture.asset(AssetPaths.logosvg, height: 60, width: 60),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/ui/utilities/asset_paths.dart
@@ -0,0 +1,5 @@
+class AssetPaths {
+  static const String _imageUrl = 'assets/images/';
+  static const String backgroundSvg = '$_imageUrl/background.svg';
+  static const String logosvg = '$_imageUrl/logo.svg';
+}
